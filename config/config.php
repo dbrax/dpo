568b8b345028e2023c337cd054248f77023ef684
Emmanuel <epmnzava@gmail.com>
2024-04-07 21:00:20 +0300

    updated code

--- config/config.php
+++ config/config.php
@@ -1,4 +1,11 @@
 <?php
 
 
-return [];
+return [
+
+    "endpoint" => env("DPO_ENDPOINT", "https://secure.3gdirectpay.com/API/v6/"),
+
+    "payment_url" => env("DPO_PAYMENT_URL", "https://secure.3gdirectpay.com/payv3.php?ID="),
+
+    "company_token" => env("DPO_COMPANY_TOKEN")
+];
--- src/Dpo.php
+++ src/Dpo.php
@@ -4,4 +4,9 @@ namespace Epmnzava\Dpo;
 
 class Dpo
 {
+
+    public function createToken()
+    {
+        $xmlData = "<?xml version=\"1.0\" encoding=\"utf-8\"?><API3G><CompanyToken>57466282-EBD7-4ED5-B699-8659330A6996</CompanyToken><Request>createToken</Request><Transaction><PaymentAmount>450.00</PaymentAmount><PaymentCurrency>USD</PaymentCurrency><CompanyRef>49FKEOA</CompanyRef><RedirectURL>http://www.domain.com/payurl.php</RedirectURL><BackURL>http://www.domain.com/backurl.php </BackURL><CompanyRefUnique>0</CompanyRefUnique><PTL>5</PTL></Transaction><Services><Service><ServiceType>45</ServiceType><ServiceDescription>Flight from Nairobi to Diani</ServiceDescription><ServiceDate>2013/12/20 19:00</ServiceDate></Service></Services></API3G>";
+    }
 }
