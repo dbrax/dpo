1338e3520401b51c42c013cc40c6dc6d9c3a836b
Emmanuel <epmnzava@gmail.com>
2024-04-08 11:05:03 +0300

    updated code

--- src/Dpo.php
+++ src/Dpo.php
@@ -26,7 +26,23 @@ class Dpo
     {
         $xmlData = "<?xml version=\"1.0\" encoding=\"utf-8\"?><API3G><CompanyToken>" . config("dpo.company_token") . "</CompanyToken><Request>createToken</Request><Transaction><PaymentAmount>" . $this->amount . "</PaymentAmount><PaymentCurrency>" . $this->currency . "</PaymentCurrency><CompanyRef>" . $this->reference . "</CompanyRef><RedirectURL>" . config("dpo.redirect_url") . "</RedirectURL><BackURL>" . config("dpo.back_url") . " </BackURL><CompanyRefUnique>" . $this->company_ref_unique . "</CompanyRefCompanyRefUnique><PTL>5</PTL></Transaction><Services><Service><ServiceType>" . $this->service_type . "</ServiceType><ServiceDescription>" . $this->service_description . "</ServiceDescription><ServiceDate>" . now() . "</ServiceDate></Service></Services></API3G>";
 
-        return $xmlData;
+
+        $ch = curl_init();
+
+        if (!$ch) {
+            die("Couldn't initialize a cURL handle");
+        }
+        curl_setopt($ch, CURLOPT_URL, config("dpo.endpoint"));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/xml'));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $xmlData);
+
+        $result = curl_exec($ch);
+
+        curl_close($ch);
+        return $result;
     }
 
     public function verify_token()
