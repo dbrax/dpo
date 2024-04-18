66a8d69eccd61af5a35748b71042a716148a12fe
Emmanuel <epmnzava@gmail.com>
2024-04-18 15:07:45 +0300

    removed backurl space

--- src/Dpo.php
+++ src/Dpo.php
@@ -18,7 +18,7 @@ class Dpo
     {
 
 
-        $xmlData = "<?xml version=\"1.0\" encoding=\"utf-8\"?><API3G><CompanyToken>" . config("dpo.company_token") . "</CompanyToken><Request>createToken</Request><Transaction><PaymentAmount>" . $amount . "</PaymentAmount><PaymentCurrency>" . $currency . "</PaymentCurrency><CompanyRef>" . $reference . "</CompanyRef><RedirectURL>" . $redirect_url . "</RedirectURL><BackURL>" . config("dpo.back_url") . " </BackURL><CompanyRefUnique>0</CompanyRefUnique><PTL>5</PTL></Transaction><Services><Service><ServiceType>" . $service_type . "</ServiceType><ServiceDescription>" . $service_description . "</ServiceDescription><ServiceDate>" . now()->format("Y/m/d H:i") . "</ServiceDate></Service></Services></API3G>";
+        $xmlData = "<?xml version=\"1.0\" encoding=\"utf-8\"?><API3G><CompanyToken>" . config("dpo.company_token") . "</CompanyToken><Request>createToken</Request><Transaction><PaymentAmount>" . $amount . "</PaymentAmount><PaymentCurrency>" . $currency . "</PaymentCurrency><CompanyRef>" . $reference . "</CompanyRef><RedirectURL>" . $redirect_url . "</RedirectURL><BackURL>" . config("dpo.back_url") . "</BackURL><CompanyRefUnique>0</CompanyRefUnique><PTL>5</PTL></Transaction><Services><Service><ServiceType>" . $service_type . "</ServiceType><ServiceDescription>" . $service_description . "</ServiceDescription><ServiceDate>" . now()->format("Y/m/d H:i") . "</ServiceDate></Service></Services></API3G>";
 
 
         $ch = curl_init();
